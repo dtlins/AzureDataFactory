e4512589d54b7942407229c6b3b547d1e26b71db
CurnanReidy <CurnanReidy@CR-138-LP>
2016-12-12 11:57:37 +0000

    Added SaveToFile

---
+++ ScriptGenerator/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptGenerator
+{
+    public static class Extensions
+    {
+        
+        public static void SaveToFile(this string jsonStr, string folder)
+        {
+            if (string.IsNullOrEmpty(jsonStr))
+                return;
+
+            Directory.CreateDirectory(folder);
+            dynamic ssd = JsonConvert.DeserializeObject(jsonStr);
+            var path = folder + ssd.name + ".json";
+            //File.Create(path);
+
+            using (TextWriter sw = new StreamWriter(path, true))
+            {
+                sw.Write(jsonStr);
+            }
+        }
+    }
+}
--- ScriptGenerator/Program.cs
+++ ScriptGenerator/Program.cs
@@ -11,24 +11,31 @@ namespace ScriptGenerator
 {
     internal class Program
     {
+        private static string _folder = Environment.CurrentDirectory + "\\files\\";
+
         static void Main(string[] args)
         {
-            var filepath = ConfigurationManager.AppSettings.GetValues("tablesCSVLocation").FirstOrDefault() ?? Environment.CurrentDirectory + "\\tables.csv";
+            var filepath = ConfigurationManager.AppSettings.GetValues("tablesCSVLocation")?.FirstOrDefault() ?? Environment.CurrentDirectory + "\\tables.csv";
             string results = string.Empty;
 
             var csv = new CSVfile(filepath);
 
+            var delimiter = '|';
+
             //foreach (var table in csv.Tables)
             //{
-            //    //results += Templates.GenerateSourceTable(table) + Environment.NewLine;
-            //    results += Templates.GenerateDestinationTable(table) + Environment.NewLine;
-            //    //results += Templates.AzureSqlDWTableStaging(table) + Environment.NewLine;
+            //    results += Templates.GenerateSourceTable(table) + delimiter;
+            //    results += Templates.GenerateDestinationTable(table) + delimiter;
+            //    results += Templates.AzureSqlDWTableStaging(table) + delimiter;
             //}
 
-            var onlyUpdatesInLastXDays = TimeSpan.FromDays(1);
-            results += Templates.GenerateCopyPipeline(DateTime.UtcNow, DateTime.UtcNow, csv, LinkedService.LinkedServices["All"], onlyUpdatesInLastXDays);
+            //Directory.Delete(_folder, true);
+            //results.Split(delimiter).ToList().ForEach(s => s.SaveToFile(_folder));
 
-            //results += Templates.GenerateUpsertStoredProcedures(csv);
+            //var onlyUpdatesInLastXDays = TimeSpan.FromDays(1);
+            //results += Templates.GenerateCopyPipeline(DateTime.UtcNow, DateTime.UtcNow, csv, LinkedService.LinkedServices["All"], onlyUpdatesInLastXDays);
+
+            results += Templates.GenerateUpsertStoredProcedures(csv);
             //results += Templates.GenerateGetStoredProcedures(csv);
 
             File.WriteAllText(Environment.CurrentDirectory + "\\results.txt", results);
